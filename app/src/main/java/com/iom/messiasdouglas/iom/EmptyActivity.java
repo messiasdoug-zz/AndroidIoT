3cc36fa2d320cd2a995deb61343ba2134ec6d7d0
Messias Freitas <messiasdouglas@outlook.com>
2016-11-04 03:26:20 -0200

    subindo alteração para reconhecimento de voz

--- app/src/main/java/com/iom/messiasdouglas/iom/EmptyActivity.java
+++ app/src/main/java/com/iom/messiasdouglas/iom/EmptyActivity.java
@@ -1,77 +1,157 @@
 package com.iom.messiasdouglas.iom;
 
-import android.annotation.TargetApi;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.app.VoiceInteractor;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import android.graphics.BitmapFactory;
-import android.os.Build;
 import android.speech.RecognizerIntent;
-import android.support.annotation.RequiresApi;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.app.NotificationCompat;
+import android.util.Log;
+import android.view.WindowManager;
+import android.widget.Toast;
 
-import java.util.List;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
-public class EmptyActivity extends AppCompatActivity {
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Locale;
+
+public class EmptyActivity extends AppCompatActivity implements TextToSpeech.OnInitListener, TextToSpeech.OnUtteranceCompletedListener {
+
+    TextToSpeech mTextToSpeech;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_empty);
 
-        displaySpeechRecognizer();
-//        geraNotificacao();
-    }
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
 
-    private void geraNotificacao() {
-        NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-
-        Intent it = new Intent(this, MensagemActivity.class);
-        it.putExtra("msg", "Obrigado por ver minha notificação!");
-        PendingIntent p = PendingIntent.getActivity(this, 0, it, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
-        builder.setContentTitle("Título");
-        builder.setContentText("Minha primeira notificação!");
-        builder.setSmallIcon(R.mipmap.ic_launcher);
-        builder.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher));
-        builder.setDefaults(Notification.DEFAULT_ALL);
-        builder.setAutoCancel(true);
-        builder.setContentIntent(p);
-
-        nm.notify(1, builder.build());
+        mTextToSpeech = new TextToSpeech(this, this);
     }
 
     private static final int SPEECH_REQUEST_CODE = 0;
 
-    // Create an intent that can start the Speech Recognizer activitys
-    private void displaySpeechRecognizer() {
-        //        Intent intent = new Intent(this, MyService.class);
-        //        stopService(intent);
-        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        // Start the activity, the intent will be populated with the speech text
-        startActivityForResult(intent, SPEECH_REQUEST_CODE);
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            int result = mTextToSpeech.setLanguage(Locale.getDefault());
+
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "This Language is not supported");
+            } else {
+                speakOut();
+            }
+        } else {
+            Log.e("TTS", "Initilization Failed!");
+        }
     }
 
-    // This callback is invoked when the Speech Recognizer returns.
-// This is where you process the intent and extract the speech text from the intent.
     @Override
-    protected void onActivityResult(int requestCode, int resultCode,
-                                    Intent data) {
-        if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
-            List<String> results = data.getStringArrayListExtra(
-                    RecognizerIntent.EXTRA_RESULTS);
-            String spokenText = results.get(0);
-            // Do something with spokenText
-            Intent intent = new Intent(this, MyService.class);
-            stopService(intent);
+    public void onUtteranceCompleted(String utteranceId) {
+        promptSpeechInput();
+    }
+
+    private void speakOut() {
+        mTextToSpeech.setOnUtteranceCompletedListener(this);
+
+        HashMap<String, String> myHashAlarm = new HashMap<String, String>();
+        myHashAlarm.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "SOME MESSAGE");
+
+        mTextToSpeech.speak("Você deseja abrir o portão?", TextToSpeech.QUEUE_FLUSH, myHashAlarm);
+    }
+
+
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+
+    private void promptSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Você deseja abrir o portão?");
+
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(), "Fala não disponível",
+                    Toast.LENGTH_SHORT).show();
         }
+    }
+
+    /**
+     * Receiving speech input
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+                    switch (result.get(0).toUpperCase()) {
+                        case "SIM":
+                            sendRequest();
+                            break;
+                        case "DESEJO":
+                            sendRequest();
+                            break;
+                        case "NÃO":
+                            Intent intent = new Intent(this, MyService.class);
+                            stopService(intent);
+                            break;
+                        default:
+                            speakOut();
+                            break;
+                    }
+                } else {
+                    speakOut();
+                }
+                break;
+            }
+            default:
+                promptSpeechInput();
+                break;
+        }
+    }
+
+    private void sendRequest() {
+        final String url = "http://api-iot-send-command.azurewebsites.net/iot/api/sendcommand";
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        // prepare the Request
+        JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // display response
+                        Log.d("Response ", response.toString());
+                        Toast.makeText(getApplicationContext(), response.toString(),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Error.Response ", error.getMessage());
+                        Toast.makeText(getApplicationContext(), error.toString(),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+
+        // add it to the RequestQueue
+        queue.add(getRequest);
     }
 }
